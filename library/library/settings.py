766c34a8f9e9e2b95f1bb4244272aa4e1e841953
Filip Kovačević <filip.kovacevic111@gmail.com>
2021-11-06 21:39:50 +0100

    adds rest_framework

--- library/library/settings.py
+++ library/library/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'libraryapi.apps.LibraryapiConfig'
+    'libraryapi.apps.LibraryapiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
