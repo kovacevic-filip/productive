1b6a0efef7ca3226d5983f12068f1a411c69d71e
Filip Kovačević <filip.kovacevic111@gmail.com>
2021-11-07 15:01:49 +0100

    adds views

--- library/libraryapi/views.py
+++ library/libraryapi/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import AuthorSerializer, BookSerializer, LoanSerializer
+from .models import *
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all().order_by("name")
+    serializer_class = AuthorSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class LoanViewSet(viewsets.ModelViewSet):
+    queryset = Loan.objects.all()
+    permission_classes = (user for user in queryset)
+    serializer_class = LoanSerializer
